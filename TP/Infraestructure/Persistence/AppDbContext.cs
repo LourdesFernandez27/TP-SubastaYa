43c5e5a976fc0e6f8c17d4eaf02664f1b291b89c
LourdesFernandez27 <lourdesjanet_fernandez@hotmail.com>
2026-09-06 21:15:22 -0300

    Actualizar AppDbContext.cs

--- TP/Infraestructure/Persistence/AppDbContext.cs
+++ TP/Infraestructure/Persistence/AppDbContext.cs
@@ -14,5 +14,36 @@ namespace Infraestructure.Persistence
         public DbSet<Categoria> Categorias { get; set; }
         public DbSet<Subasta> Subastas { get; set; }
         public DbSet<Puja> Pujas { get; set; }
+
+						protected override void OnModelCreating(ModelBuilder moderlBuilder)
+						{
+								modelBuilder.Entity<Subasta>(entity =>
+								{
+								entity.ToTable("Subasta");
+								entity.HasKey(e => e.Id);
+								entity.Property(e => e.Id).ValueGeneratedOnAdd();
+								entity.HasMany(s => s.Pujas)
+								.WithOne(p => p.Subasta)
+								.HasForeignKey(p => p.SubastaId);
+								});
+								
+								modelBuilder.Entity<Usuario>(entity =>
+								{
+								entity.ToTable("Usuario");
+								entity.HasKey(e => e.Id);
+								entity.HasOne(u => u.Billetera)
+								.WithOne(b => b.Usuario)
+								.HasForeignKey<Billetera>(b => b.UsuarioId);
+								});
+							
+								modelBuilder.Entity<Puja>(entity =>
+								{
+								entity.ToTable("Puja");
+								entity.HasKey(e => e.Id);
+								entity.HasOne(p => p.Subasta)
+								.WithMany(s => s.Pujas)
+								.HasForeignKey(p => p.SubastaId);
+								});							
+						}
     }
 }
