ed930ea1ac0e8a38eb153c003fb7107298491a01
LourdesFernandez27 <lourdesjanet_fernandez@hotmail.com>
2026-09-13 10:01:59 -0300

    controllers

--- TP/TP-Subasta/Program.cs
+++ TP/TP-Subasta/Program.cs
@@ -18,6 +18,7 @@ builder.Services.AddDbContext<AppDbContext>(option => option.UseSqlServer(connec
 */
 builder.Services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer(connectionString));
 
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
